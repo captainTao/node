e23574fdb58a023448da3acddf3bbdc45f0e6457
captainTao <578628438@qq.com>
2018-05-12 18:05:19 +0800

    .

--- hello.js
+++ hello.js
@@ -1370,3 +1370,58 @@ $('xx').highlight({
 插件函数要有默认值，绑定在$.fn.<pluginName>.defaults上；
 用户在调用时可传入设定值以便覆盖默认值。
 /////////////////////////////////////// JQuery----end ////////////////////////////////////
+
+
+// 异常：
+try {
+    ...
+} catch (e) {
+    ...
+} finally {
+    ...
+}
+
+错误类型:
+JavaScript有一个标准的Error对象表示错误，还有从Error派生的TypeError、ReferenceError等错误对象。我们在处理错误时，可以通过catch(e)捕获的变量e访问错误对象：
+
+try {
+    ...
+} catch (e) {
+    if (e instanceof TypeError) {
+        alert('Type error!');
+    } else if (e instanceof Error) {
+        alert(e.message);
+    } else {
+        alert('Error: ' + e);
+    }
+}
+使用变量e是一个习惯用法，也可以以其他变量名命名，如catch(ex)。
+
+
+
+抛出错误:
+抛出错误使用throw语句。
+JavaScript允许抛出任意对象，包括数字、字符串。但是，最好还是抛出一个Error对象。
+var r, n, s;
+try {
+    s = prompt('请输入一个数字');
+    n = parseInt(s);
+    if (isNaN(n)) {
+        throw new Error('输入错误');
+    }
+    // 计算平方:
+    r = n * n;
+    console.log(n + ' * ' + n + ' = ' + r);
+} catch (e) {
+    console.log('出错了：' + e);
+}
+
+
+当我们用catch捕获错误时，一定要编写错误处理语句：
+var n = 0, s;
+try {
+    n = s.length;
+} catch (e) {
+    console.log(e);
+}
+console.log(n);
