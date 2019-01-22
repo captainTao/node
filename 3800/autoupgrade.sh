ef8c41c1c73b20bdf62f9b59d876d4b398ea08b5
captainTao <578628438@qq.com>
2019-01-22 20:20:32 +0800

    sed awk cut

---
+++ 3800/autoupgrade.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# last update:2019/1/22,captain wang 
+opkg update
+for ipk in $(opkg list-upgradable | awk '$1!~/^kmod|^Multiple/{print $1}'); do
+	opkg upgrade $ipk
+done
+# 另外一种更新方法：
+# opkg list-upgradable | awk -F ' - ' '{print $1}' | xargs opkg upgrade
+
+cd ~
+# 更新chinadns
+wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /etc/chinadns_chnroute.txt
+/etc/init.d/shadowsocks restart
+
+# China-List
+curl -L -o generate_dnsmasq_chinalist.sh https://github.com/cokebar/openwrt-scripts/raw/master/generate_dnsmasq_chinalist.sh
+chmod +x generate_dnsmasq_chinalist.sh
+# GfwList
+curl -L -o gfwlist2dnsmasq.sh https://github.com/cokebar/gfwlist2dnsmasq/raw/master/gfwlist2dnsmasq.sh
+chmod +x gfwlist2dnsmasq.sh
+
+# China-list  114可以换成ISP的dns,这样运营商分配最近的服务器
+sh generate_dnsmasq_chinalist.sh -d 114.114.114.114 -p 53 -o /etc/dnsmasq.d/accelerated-domains.china.conf
+# GfwList
+sh gfwlist2dnsmasq.sh -d 127.0.0.1 -p 5311 -o /etc/dnsmasq.d/dnsmasq_gfwlist.conf
+# Restart dnsmasq
+/etc/init.d/dnsmasq restart
