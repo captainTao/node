d92a119d3c94953c12237df88bcbea70f6f81144
captainTao <578628438@qq.com>
2019-10-11 22:53:09 +0800

    1.搭建github.io+hexo博客 2.本地起一个node server

---
+++ nodeserver/server.js
@@ -0,0 +1,13 @@
+const Koa = require('koa')
+const path = require('path')
+const staticFiles = require('koa-static')
+// 定义一个内核
+const app = new Koa()
+// 读取本地文件的目录
+let main = staticFiles(path.resolve(__dirname, "./tools"))
+// 内核读取哪些文件，这里为html
+app.use(main, {extensions: ['html']})
+// 启动服务 监听
+app.listen(4050, () =>{
+    console.log('server is running at http://localhost:4050')
+})
