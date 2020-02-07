222fe9036c39c7653adc666f886cc7e94c44f063
captainTao <578628438@qq.com>
2020-02-07 17:56:30 +0800

    1.添加nginx和virtual op的一些注意事项 2.完善了auto_intall.sh后续执行操作 3.备份了auto_install原来的文件

--- 3800/auto_install_bak.sh
+++ 3800/auto_install_bak.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# according to cokebar's shell script, symlink: https://cokebar.info/archives/664
-# update by captain, on 02/24/2019
-
-opkg update
-opkg install coreutils-base64 ca-certificates ca-bundle curl wget libustream-mbedtls  #如果要用https,就需要tls
 
 INSTALLED=$(opkg list-installed)
 
@@ -35,57 +30,36 @@ opkg update
 if echo "$INSTALLED" | grep -q "luci"; then
 	LuCI=yes
 fi
+
 read -p "Install the ChinaDNS [Y/n]?" INS_CD
 read -p "Install the DNS-Forwarder [Y/n]?" INS_DF
 read -p "Install the shadowsocks-libev [Y/n]?" INS_SS
-read -p "Install the simple-obfs [Y/n]?" INS_SO
 read -p "Install the ShadowVPN [Y/n]?" INS_SV
-read -p "Install the usb-storage [Y/n]?" INS_US   #symlink: https://wizju.com/post/123/
-read -p "Install the usb-storage-3.0 addition [Y/n]?" INS_USS
-read -p "Install the usb-printer [Y/n]?" INS_UP
 
 if echo ${INS_CD} | grep -qi "^y"; then
 	opkg install ChinaDNS
-	if [ "$LuCI" = "yes" ]; then
+	if [[ "$LuCI" = "yes" ]]; then
 		opkg install luci-app-chinadns
 	fi
 fi
 
 if echo ${INS_DF} | grep -qi "^y"; then
 	opkg install dns-forwarder
-	if [ "$LuCI" = "yes" ]; then
+	if [[ "$LuCI" = "yes" ]]; then
 		opkg install luci-app-dns-forwarder
 	fi
 fi
 
 if echo ${INS_SS} | grep -qi "^y"; then
 	opkg install shadowsocks-libev
-	if [ "$LuCI" = "yes" ]; then
+	if [[ "$LuCI" = "yes" ]]; then
 		opkg install luci-app-shadowsocks
 	fi
 fi
 
-if echo ${INS_SO} | grep -qi "^y"; then
-	opkg install simple-obfs
-fi
-
 if echo ${INS_SV} | grep -qi "^y"; then
 	opkg install ShadowVPN
-	if [ "$LuCI" = "yes" ]; then
+	if [[ "$LuCI" = "yes" ]]; then
 		opkg install luci-app-shadowvpn
 	fi
 fi
-
-if echo ${INS_US} | grep -qi "^y"; then
-	opkg install kmod-usb-core kmod-scsi-core kmod-scsi-generic kmod-usb-uhci kmod-usb-ohci kmod-usb2 kmod-usb-storage kmod-usb-storage-extras kmod-fs-ext4 kmod-fs-msdos kmod-fs-ntfs kmod-fs-vfat kmod-fs-hfs kmod-fs-hfsplus mount-utils
-fi
-
-if echo ${INS_USS} | grep -qi "^y"; then
-	opkg install kmod-usb3
-fi
-
-if echo ${INS_UP} | grep -qi "^y"; then
-	opkg install kmod-usb-printer && wget -q "https://raw.githubusercontent.com/captainTao/node/master/3800/luci-app-usb-printer_svn-r9961-1_all.ipk" && opkg install luci-app-usb-printer_svn-r9961-1_all.ipk
-fi
-
-opkg install ip-full iptables-mod-tproxy ipset libpthread
